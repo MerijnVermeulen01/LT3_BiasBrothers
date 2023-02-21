a2cdf6597aad267ffcf82260845381a735051ade
Cayde7 <merijn.vermeulen@outlook.com>
2023-02-21 15:01:48 +0100

    sidebar Toegevoegt
    Heb een sidebar toegevoegt met javascipt niet veel speciaals maar dat we het niet 8 keer over moeten doen

---
+++ Frontend/Javascript/sidebar.js
@@ -0,0 +1,12 @@
+function sidebarLoad(){
+    document.write(
+        "<ul>\
+            <li><a href='ontwikkelActies.html'>Ontwikkelacties</a></li>\
+            <li><a href='denkvalkuilen.html'>Denkvalkuilen</a></li>\
+            <li><a href='mijnBiasesPagina.html'>Mijn Biases</a></li>\
+            <li><a href='aanpassingsvermogen.html'>Aanpassingsvermogen</a></li>\
+            <li><a href='codex.html'>Codex</a></li>\
+            <li><a href='downloadGegevensPagina.html'>Download gegevens</a></li>\
+            </ul>"
+    );
+}
