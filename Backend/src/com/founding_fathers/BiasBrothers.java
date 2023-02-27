8fb72663df2912596a3dd88828adb8f3787fdc7c
Cayde7 <43235246+Cayde7@users.noreply.github.com>
2023-02-27 11:22:24 +0100

    Database Connection
    Het is af je kan er in werken :D

--- Backend/src/com/founding_fathers/BiasBrothers.java
+++ Backend/src/com/founding_fathers/BiasBrothers.java
@@ -1,7 +1,34 @@
 package com.founding_fathers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class BiasBrothers {
     public static void main(String[] args){
         System.out.println("Hello world!");
+
+        String url = "jdbc:mysql://localhost:3308/biasbrothers";
+        String username = "root";
+        String password = "Thefoundingfathers";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url,username,password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM codex");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2));
+            }
+
+            connection.close();
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
