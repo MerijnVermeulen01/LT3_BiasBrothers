d0479301fa65c67fad50c0c66f2881dfcaf730f1
Shawn <shawnodonoghue@icloud.com>
2023-03-07 14:56:33 +0100

    update js

--- Frontend/Javascript/pages.js
+++ Frontend/Javascript/pages.js
@@ -4,32 +4,29 @@ const pressedButtons = [];
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
 
-        if (button.classList.contains('activeButton')) {
-            button.classList.remove('activeButton');
-            button.removeChild(button.querySelector('.circle'));
-            const index = pressedButtons.indexOf(button);
+        if(pressedButtons.length <= 3){
+            if (button.classList.contains('activeButton')) {
+                button.classList.remove('activeButton');
 
-            if (index > -1) {
-                pressedButtons.splice(index, 1);
-
+                // button.removeChild(button.querySelector('.circle'));
+                const index = pressedButtons.indexOf(button);
+                if (index > -1) {
+                    pressedButtons.splice(index, 1);
+                }
             }
-        }
 
-        else {
-            button.classList.add('activeButton');
-            const circle = document.createElement('div');
-
-            circle.classList.add('circle');
-            console.log(pressedButtons);
-            circle.textContent = pressedButtons.length + 1;
-            console.log(circle);
-            button.appendChild(circle);
-            circle.style.top = button.offsetTop + 'px';
-            circle.style.left = button.offsetLeft + 'px';
-            pressedButtons.push(button);
-
-            console.log(pressedButtons.length);
-
+            else {
+                button.classList.add('activeButton');
+                // const circle = document.createElement('div');
+                // circle.classList.add('circle');
+                // console.log(pressedButtons);
+                // circle.textContent = pressedButtons.length + 1;
+                // console.log(circle);
+                // button.appendChild(circle);
+                // circle.style.top = button.offsetTop + 'px';
+                // circle.style.left = button.offsetLeft + 'px';
+                pressedButtons.push(button);
+            }
         }
     });
 });
