a4b11298afe1d899ac4e2f1667199fd21f665f75
EvanSchriek <evan.schriekenberg@student.hu.nl>
2023-03-14 10:53:53 +0100

    Geen Pass/Localhost veranderd voor iedereen

--- Backend/src/main/java/com/founding_fathers/pages/controller/DatabaseController.java
+++ Backend/src/main/java/com/founding_fathers/pages/controller/DatabaseController.java
@@ -4,31 +4,31 @@ import java.sql.*;
 
 public class DatabaseController {
 
-//    This function can go but its for test purpose here still
-//    public DatabaseController(){
-//        String url = "jdbc:mysql://localhost:3308/biasbrothers";
-//        String username = "root";
-//        String password = "Thefoundingfathers";
-//
-//        try {
-//            Class.forName("com.mysql.cj.jdbc.Driver");
-//
-//            Connection connection = DriverManager.getConnection(url,username,password);
-//
-//            System.out.println(connection);
-//
-//            Statement statement = connection.createStatement();
-//
-//            ResultSet resultSet = statement.executeQuery("SELECT * FROM bias");
-//
-//            while (resultSet.next()){
-//                System.out.println(resultSet.getInt(1) + " " + resultSet.getInt(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
-//            }
-//
-//        } catch (Exception e){
-//            System.out.println(e);
-//        }
-//    }
+
+    public DatabaseController(){
+        String url = "jdbc:mysql://localhost:3306/biasbrothers";
+        String username = "root";
+        String password = "";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url,username,password);
+
+            System.out.println(connection);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM bias");
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getInt(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
+            }
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
 
     /**
      * This function connects to the database.
