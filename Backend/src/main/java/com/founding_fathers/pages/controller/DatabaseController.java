10828708551629539638fcde5f74e8feb8179722
Shawn <shawnodonoghue@icloud.com>
2023-03-14 14:45:35 +0100

    Update DatabaseController.java

--- Backend/src/main/java/com/founding_fathers/pages/controller/DatabaseController.java
+++ Backend/src/main/java/com/founding_fathers/pages/controller/DatabaseController.java
@@ -3,33 +3,6 @@ package com.founding_fathers.pages.controller;
 import java.sql.*;
 
 public class DatabaseController {
-
-
-    public DatabaseController(){
-        String url = "jdbc:mysql://localhost:3306/biasbrothers";
-        String username = "root";
-        String password = "";
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection connection = DriverManager.getConnection(url,username,password);
-
-            System.out.println(connection);
-
-            Statement statement = connection.createStatement();
-
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM bias");
-
-            while (resultSet.next()){
-                System.out.println(resultSet.getInt(1) + " " + resultSet.getInt(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + " " + resultSet.getString(5));
-            }
-
-        } catch (Exception e){
-            System.out.println(e);
-        }
-    }
-
     /**
      * This function connects to the database.
      * The values that you need are url(the localhost:port/databasename), Username form the database and the password of the database.
