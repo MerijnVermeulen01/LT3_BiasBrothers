4b673ab200b5846514452bce008e577d331ffcf5
Cayde7 <43235246+Cayde7@users.noreply.github.com>
2023-03-15 21:42:07 +0100

    update

--- Frontend/Javascript/fetchData.js
+++ Frontend/Javascript/fetchData.js
@@ -2,6 +2,7 @@
 fetch('http://localhost:7070/MyBias')
     .then(repsone => repsone.json())
     .then(data => {
+        console.log(data);
         data.forEach(post => {
             // "name".insertAdjacentHTML('beforeend', `"In which HTML tag u want it" ${post."point u want in the JSON"} "In which HTML tag u want it"`);
         })
