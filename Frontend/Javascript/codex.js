f70c4c2957876615fed4a58dbd9a8013ad07688b
PeterRuizen <peter.ruizendaal@student.hu.nl>
2023-03-16 12:39:10 +0100

    js for making divs

--- Frontend/Javascript/codex.js
+++ Frontend/Javascript/codex.js
@@ -1,8 +1,28 @@
-const codex = document.querySelector('.codex'');
+const newDiv document.createElement("div");
+const newDivText document.createElement("div")
+const newH3 document.createElement("h3");
 
-const div = document.createElement("div");
-div.classList.add('codexTitel');
-const text = "test";
-const divContent=document.createTextNode(text);
-div.appendChild(divContent);
-document.body.appendChild(div);
+
+// const "name" = document.querySelector('"html where u want it"');
+fetch('http://localhost:7070/MyBias')
+    .then(repsone => repsone.json())
+    .then(data => {
+        data.forEach(post => {
+            loop(title,text)
+            {
+                title.insertAdjacentHTML('beforeend', `"In which HTML tag u want it" ${post."point u want in the JSON"} "In which HTML tag u want it"`);
+                text.insertAdjacentHTML('beforeend', `"In which HTML tag u want it" ${post."point u want in the JSON"} "In which HTML tag u want it"`);
+            }
+
+            // "name".insertAdjacentHTML('beforeend', `"In which HTML tag u want it" ${post."point u want in the JSON"} "In which HTML tag u want it"`);
+        })
+    })
+function fillDiv(title, text) {
+    let titles = document.createTextNode(title);
+    let text = document.createTextNode(text);
+
+    newH3.appendChild(titles);
+    newDivText.appendChild(text);
+
+    docment.getElementById('codexBox').appendChild(newDiv);
+}
