68fbb9bcc9e99037b1bee14e9457608e9ceea25f
Cayde7 <43235246+Cayde7@users.noreply.github.com>
2023-04-19 09:13:48 +0200

    admin portal

--- Frontend/Javascript/adminPortal/adminPortalMain.js
+++ Frontend/Javascript/adminPortal/adminPortalMain.js
@@ -1,16 +1,45 @@
 let mountains = [
-    { name: "TunnelVisie", description: "Tunnelvisie beschrijving", buttons: '<a href="insertEditBiases.html?edit=true&id=4">Edit</a><a id="deleteButton" href="delete.html">Delete</a>' },
-    { name: "DenkValkuilen", description: "DenkValkuilen beschrijving", buttons: '<a href="insertEditBiases.html?edit=true&id=4">Edit</a><a id="deleteButton" href="delete.html">Delete</a>' },
-    { name: "YEET", description: "YEET beschrijving", buttons: '<a href="insertEditBiases.html?edit=true&id=4">Edit</a><a id="deleteButton" href="delete.html">Delete</a>' },
-  ];
+    { name: "Tunnelvisie", description: "Tunnelvisie beschrijving" },
+    { name: "Denkvalkuilen", description: "Denkvalkuilen beschrijving" },
+];
+
+let count = 1;
+
 function generateTable(table, data) {
+    
     for (let element of data) {
         let row = table.insertRow();
+
         for (key in element) {
             let cell = row.insertCell();
             let text = document.createTextNode(element[key]);
+            // if(count % 2 == 0){
+            //     cell.classList.add("descriptionText");
+            // } else {
+            //     cell.classList.add("tableTitle");
+            // }
+            console.log(mountains[0].name);
             cell.appendChild(text);
         }
+        const cellButton = row.insertCell();
+        cellButton.classList.add("buttonArea");
+        
+        const editButton = document.createElement("a");
+        editButton.href = "insertEditThinkingTraps.html?edit=true&id=" + count;
+        const editTextButton = document.createTextNode("Edit");
+        editButton.appendChild(editTextButton);
+
+
+        const deleteButton = document.createElement("a");
+        deleteButton.href = "delete.html?mode=thinkingtraps&id=" + count;
+        const deleteTextButton = document.createTextNode("Delete");
+        deleteButton.setAttribute("id", "deleteButton");
+        deleteButton.appendChild(deleteTextButton);
+        
+        cellButton.appendChild(editButton);
+        cellButton.appendChild(deleteButton);
+        count++;
+
     }
 }
 
