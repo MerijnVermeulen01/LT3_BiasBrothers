94a624bf2af495d97cd35f96a013f2a409986c2b
Cayde7 <merijn.vermeulen@outlook.com>
2023-04-19 10:01:43 +0200

    frontend admin portal af

--- Frontend/Javascript/adminPortal/adminPortalMain.js
+++ Frontend/Javascript/adminPortal/adminPortalMain.js
@@ -1,37 +1,66 @@
-let mountains = [
-    { name: "Tunnelvisie", description: "Tunnelvisie beschrijving" },
-    { name: "Denkvalkuilen", description: "Denkvalkuilen beschrijving" },
-];
-
+const queryString = window.location.href;
 let count = 1;
+if (queryString.includes("thinkingTrapsEdit")){
+    fetchDataThinkingTraps();
+}else if (queryString.includes("biasesEdit")){
+    fetchDataBiases();
+}
+
+function fetchDataThinkingTraps(){
+    // fetch('http://localhost:7070/')
+    // .then(repsone => repsone.json())
+    // .then(data => {
+    // let table = document.querySelector("table");
+    //     generateTable(table, data);
+    // });
+}
+
+function fetchDataBiases(){
+    // fetch('http://localhost:7070/')
+    // .then(repsone => repsone.json())
+    // .then(data => {
+    // let table = document.querySelector("table");
+    //     generateTable(table, data);
+    // });
+}
 
 function generateTable(table, data) {
     
     for (let element of data) {
         let row = table.insertRow();
-
         for (key in element) {
             let cell = row.insertCell();
             let text = document.createTextNode(element[key]);
-            // if(count % 2 == 0){
-            //     cell.classList.add("descriptionText");
-            // } else {
-            //     cell.classList.add("tableTitle");
-            // }
-            console.log(mountains[0].name);
+
+            if(key == "description"){
+                cell.classList.add("descriptionText");
+            }else {
+                cell.classList.add("tableTitle");
+            }
+            
             cell.appendChild(text);
         }
         const cellButton = row.insertCell();
         cellButton.classList.add("buttonArea");
         
+        let fileNameEdit = "";
+        let deletePath = "";
+
+        if(queryString.includes("thinkingTrapsEdit")){
+            fileNameEdit = "insertEditThinkingTraps";
+            deletePath = "thinkingtraps";
+        }else if (queryString.includes("biasesEdit")){
+            fileNameEdit = "insertEditBiases";
+            deletePath = "biases";
+        }
         const editButton = document.createElement("a");
-        editButton.href = "insertEditThinkingTraps.html?edit=true&id=" + count;
+        editButton.href = fileNameEdit + ".html?edit=true&id=" + count;
         const editTextButton = document.createTextNode("Edit");
         editButton.appendChild(editTextButton);
 
 
         const deleteButton = document.createElement("a");
-        deleteButton.href = "delete.html?mode=thinkingtraps&id=" + count;
+        deleteButton.href = "delete.html?mode=" + deletePath + "&id=" + count;
         const deleteTextButton = document.createTextNode("Delete");
         deleteButton.setAttribute("id", "deleteButton");
         deleteButton.appendChild(deleteTextButton);
@@ -42,7 +71,3 @@ function generateTable(table, data) {
 
     }
 }
-
-let table = document.querySelector("table");
-let data = Object.keys(mountains[0]);
-generateTable(table, mountains);
