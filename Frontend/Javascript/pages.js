586f2674e1855f31676c9adccdf3f23f843667f3
JohanTheGr8 <jaron.rombeek@student.hu.nl>
2023-05-19 12:48:25 +0200

    Update timer
    Timer-bug for biaspage fixed. Made seperate .js file for timer.

--- Frontend/Javascript/pages.js
+++ Frontend/Javascript/pages.js
@@ -60,29 +60,6 @@ area.forEach((text) => {
                 activeTextareas.push(text);
             }
         }
-        
+
     });
 });
-
-
-const startingMinutes = 10;
-let time = startingMinutes*60;
-
-const countDownEl = document.getElementById('timer');
-
-setInterval(updateTimer, 1000);
-
-function updateTimer(){
-    const minutes = Math.floor(time/60);
-    let seconds = time % 60;
-
-    seconds = seconds<10?'0'+seconds:seconds;
-
-    countDownEl.innerHTML=`${minutes}:${seconds}`;
-    if(time == 0){
-        countDownEl.innerHTML='0:00';
-    }
-    else{
-     time--;
-    }
-}
