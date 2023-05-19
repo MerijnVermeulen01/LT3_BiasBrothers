18f3738a7b858e5572cc13d2a833ae1c8455d6bf
Shawn <shawnodonoghue@icloud.com>
2023-05-19 11:12:34 +0200

    Pdf generate fixed
    Mission accomplished

--- Frontend/Javascript/pdfGenerator.js
+++ Frontend/Javascript/pdfGenerator.js
@@ -4,9 +4,34 @@ downloadButton.addEventListener('click', () => {
 });
 
 function generatePDF() {
+    // Create a new jsPDF instance
     const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
 
-    doc.text("Hello, this is a pdf", 10, 10);
-    doc.save("sample.pdf");
+    // Add the preset pages
+    var presetPages = [
+        '/Frontend/PDF-presets/pdfOntwerp.png',
+        '/Frontend/PDF-presets/pdfOntwerp2.png',
+        '/Frontend/PDF-presets/pdfOntwerp3.png',
+        '/Frontend/PDF-presets/pdfOntwerp4.png',
+        '/Frontend/PDF-presets/pdfOntwerp5.png',
+        '/Frontend/PDF-presets/pdfOntwerp6.png'
+    ];
+
+
+    for (var i = 0; i < presetPages.length; i++) {
+        // Load the preset page
+        var pages = presetPages[i];
+
+        // Add the page to the PDF
+        doc.addPage(pages, 'PNG', 0, 0, 210, 297); // Adjust the coordinates and dimensions as needed
+        doc.addImage(pages, 'PNG', 0, 0, 210, 297); // Adjust the coordinates and dimensions as needed
+
+
+    }
+
+
+
+
+    doc.save('generated.pdf');
 }
