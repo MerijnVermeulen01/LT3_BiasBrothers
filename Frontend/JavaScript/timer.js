ee9479e13b4d3bf9edc4cecd018f68b677735189
Shawn <shawnodonoghue@icloud.com>
2023-05-22 14:20:58 +0200

    Added alarm and made timer blink when it hits 0

--- Frontend/JavaScript/timer.js
+++ Frontend/JavaScript/timer.js
@@ -1,21 +1,35 @@
-const startingMinutes = 10;
-let time = startingMinutes*60;
-
+const startingMinutes = 0.1;
+var alarmOn = new Boolean(false);
+const alarm = new Audio('/Frontend/Images/alarm2.wav');
 const countDownEl = document.getElementById('timer');
+let time = startingMinutes*60;
+var Stink = setInterval(updateTimer,1000)
 
-setInterval(updateTimer, 1000);
 
-function updateTimer(){
-    const minutes = Math.floor(time/60);
+if (alarmOn == false) {
+  Stink
+}
+
+function updateTimer() {
+    const minutes = Math.floor(time / 60);
     let seconds = time % 60;
 
-    seconds = seconds<10?'0'+seconds:seconds;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    countDownEl.innerHTML=`${minutes}:${seconds}`;
-    if(time == 0){
-        countDownEl.innerHTML='0:00';
-    }
-    else{
+    countDownEl.innerHTML = `${minutes}:${seconds}`;
+    alarmOn = true;
+    if (time == 0) {
+        countDownEl.innerHTML = '0:00';
+        countDownEl.classList.add('blink');
+        
+        alarm.volume = 0.3; // Set the volume to 30%
+        alarm.play();
+
+        setTimeout(() => {
+            alarm.pause();
+        }, 1000);
+        clearInterval(Stink)
+    } else {
         time--;
     }
 }
