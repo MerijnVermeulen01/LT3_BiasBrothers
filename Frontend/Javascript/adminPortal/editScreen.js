72ccf040ec5dd1a460ead080162d28ad50fd21e7
Cayde7 <43235246+Cayde7@users.noreply.github.com>
2023-05-26 12:08:49 +0200

    adminportaal jonge

--- Frontend/Javascript/adminPortal/editScreen.js
+++ Frontend/Javascript/adminPortal/editScreen.js
@@ -8,33 +8,33 @@ console.log(id);
 if(edit == "true"){
     console.log("Hier moet een loading function komen met fetch");
     // Hier moet een fetch komen. Met de waardes ingevult met de ID die is meegegeven.
-    // const table = document.querySelector("table");
-    // const rowTitle = table.insertRow();
-    // rowTitle.classList.add("trEditTitle");
+    const table = document.querySelector("table");
+    const rowTitle = table.insertRow();
+    rowTitle.classList.add("trEditTitle");
 
-    // const cell = rowTitle.insertCell();
-    // cell.classList.add("editTitle");
+    const cell = rowTitle.insertCell();
+    cell.classList.add("editTitle");
 
-    // const titleText = document.createTextNode("Titel:");
+    const titleText = document.createTextNode("Titel:");
     // In de input moet wel iets van text komen als je gaat editen
-    // const inputTitle = document.createElement("input");
-    // inputTitle.classList.add("titleText");
+    const inputTitle = document.createElement("input");
+    inputTitle.classList.add("titleText");
     
-    // const rowDescription = table.insertRow();
+    const rowDescription = table.insertRow();
     
-    // const cellDescription = rowDescription.insertCell();
-    // cellDescription.classList.add("editDescription");
+    const cellDescription = rowDescription.insertCell();
+    cellDescription.classList.add("editDescription");
 
-    // const descriptionText = document.createTextNode("Beschrijving:");
+    const descriptionText = document.createTextNode("Beschrijving:");
     // In de textarea moet wel iets van text komen als je gaat editen
-    // const textareaDescription = document.createElement("textarea");
-    // textareaDescription.classList.add("textarea1");
+    const textareaDescription = document.createElement("textarea");
+    textareaDescription.classList.add("textarea1");
 
-    // cell.appendChild(titleText);
-    // cell.appendChild(inputTitle);
+    cell.appendChild(titleText);
+    cell.appendChild(inputTitle);
 
-    // cellDescription.appendChild(descriptionText);
-    // cellDescription.appendChild(textareaDescription);
+    cellDescription.appendChild(descriptionText);
+    cellDescription.appendChild(textareaDescription);
 }else{
     const table = document.querySelector("table");
     const rowTitle = table.insertRow();
