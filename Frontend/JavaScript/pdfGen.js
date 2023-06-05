1488bc1a15f521d803d4a7c3500ae58219947b9f
JohanTheGr8 <jaron.rombeek@student.hu.nl>
2023-06-05 16:00:01 +0200

    All pages added.
    All pages, with data added.

--- Frontend/JavaScript/pdfGen.js
+++ Frontend/JavaScript/pdfGen.js
@@ -10,23 +10,25 @@ function generatePDF() {
 
     const apiUrls = [
         'http://localhost:7070/selfDevelopment',
-        'http://localhost:7070/getParicipantTraps'
+        'http://localhost:7070/joinedParticipantTraps',
+        'http://localhost:7070/getParicipantBias'
     ];
 
-    // Fetches all data at once - puts the data in a json format
+    // Fetches all data at once - puts the data in a json format.
     Promise.all(apiUrls.map(url => fetch(url).then(response => response.json())))
         .then(data => {
-            const [selfDevelopmentData, trapData] = data;
+            const [selfDevelopmentData, trapData, biasData] = data;
 
             // Generate the pages using the fetched data
-            // Page 1 - retrieves data from selfdevelopment.
+
+            // Page 1
             generatePage1(doc, selfDevelopmentData);
 
-            // Page 2 - retrieves data from thinking traps.
-            doc.addPage();
+            // Page 2
             generatePage2(doc, trapData);
 
-            // Page 3 - retrieves data from biases.
+            // Page 3
+            generatePage3(doc, biasData);
 
             // Save the PDF
             doc.save('Bias Brothers.pdf');
@@ -34,7 +36,7 @@ function generatePDF() {
         .catch(error => console.error(error));
 }
 
-
+// Page genarated using input-data from selfdevelopment.
 function generatePage1(doc, data) {
     data.forEach((item, index) => {
         const title = item.selfDevelopment;
@@ -43,9 +45,21 @@ function generatePage1(doc, data) {
     });
 }
 
+// Page genarated using input-data from thinking traps.
 function generatePage2(doc, data) {
+    doc.addPage();
     data.forEach((item, index) => {
-        const title = item.thinkingtraps_idThinkingTraps;
+        const title = item.thinkingTraps;
+        const description = item.description;
+        doc.text(20, 20 + index * 10, `${title}: ${description}`);
+    });
+}
+
+// Page genarated using input-data from biases.
+function generatePage3(doc, data) {
+    doc.addPage();
+    data.forEach((item, index) => {
+        const title = item.nameBias;
         const description = item.description;
         doc.text(20, 20 + index * 10, `${title}: ${description}`);
     });
