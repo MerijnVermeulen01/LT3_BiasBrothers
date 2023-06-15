6dfd0f8ac1aee8808d5fdd4b1d04a972c1259c68
PeterRuizen <peter.ruizendaal@student.hu.nl>
2023-06-15 21:40:23 +0200

    bugfixed
    gekke dubbel in meslowingdown

--- Backend/src/main/java/com/founding_fathers/pages/MeSlowingDown.java
+++ Backend/src/main/java/com/founding_fathers/pages/MeSlowingDown.java
@@ -79,50 +79,6 @@ public class MeSlowingDown extends DatabaseController {
         this.meSlowingDownSituationDescription12 = meSlowingDownSituationDescription12;
     }
 
-    public void setMeSlowingDownSituationDescription2(String meSlowingDownSituationDescription2) {
-        this.meSlowingDownSituationDescription2 = meSlowingDownSituationDescription2;
-    }
-
-    public void setMeSlowingDownSituationDescription3(String meSlowingDownSituationDescription3) {
-        this.meSlowingDownSituationDescription3 = meSlowingDownSituationDescription3;
-    }
-
-    public void setMeSlowingDownSituationDescription4(String meSlowingDownSituationDescription4) {
-        this.meSlowingDownSituationDescription4 = meSlowingDownSituationDescription4;
-    }
-
-    public void setMeSlowingDownSituationDescription5(String meSlowingDownSituationDescription5) {
-        this.meSlowingDownSituationDescription5 = meSlowingDownSituationDescription5;
-    }
-
-    public void setMeSlowingDownSituationDescription6(String meSlowingDownSituationDescription6) {
-        this.meSlowingDownSituationDescription6 = meSlowingDownSituationDescription6;
-    }
-
-    public void setMeSlowingDownSituationDescription7(String meSlowingDownSituationDescription7) {
-        this.meSlowingDownSituationDescription7 = meSlowingDownSituationDescription7;
-    }
-
-    public void setMeSlowingDownSituationDescription8(String meSlowingDownSituationDescription8) {
-        this.meSlowingDownSituationDescription8 = meSlowingDownSituationDescription8;
-    }
-
-    public void setMeSlowingDownSituationDescription9(String meSlowingDownSituationDescription9) {
-        this.meSlowingDownSituationDescription9 = meSlowingDownSituationDescription9;
-    }
-
-    public void setMeSlowingDownSituationDescription10(String meSlowingDownSituationDescription10) {
-        this.meSlowingDownSituationDescription10 = meSlowingDownSituationDescription10;
-    }
-
-    public void setMeSlowingDownSituationDescription11(String meSlowingDownSituationDescription11) {
-        this.meSlowingDownSituationDescription11 = meSlowingDownSituationDescription11;
-    }
-
-    public void setMeSlowingDownSituationDescription12(String meSlowingDownSituationDescription12) {
-        this.meSlowingDownSituationDescription12 = meSlowingDownSituationDescription12;
-    }
-
     /**
      * Function wich checks if there is already information in the page. if there is
      * it goes to the update else it goes to the insert
